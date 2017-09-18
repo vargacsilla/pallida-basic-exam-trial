1048652fc84fc8d97187e5831d964078cf3b15fc
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-18 17:52:05 +0200

    Refactoring + unnecessary files deleted

--- oddfilter/OddFilter.cs
+++ oddfilter/OddFilter.cs
@@ -7,11 +7,8 @@ namespace OddFilter
     {
         static void Main(string[] args)
         {
-            // Create an OddFilter function that takes a list as a parameter,
-            // and returns a new list with every odd element from the orignal list
             List<int> oddElements = OddFilter(new List<int> { 1, 2, 3, 4, 5 });
 
-            // In case of the example input above, the given PrintList function should print 1 3 5 
             PrintList(oddElements);
 
             Console.ReadLine();
@@ -23,10 +20,8 @@ namespace OddFilter
 
             for (int i = 0; i < list.Count; i++)
             {
-                if (oddElements[i] % 2 != 0)
-                {
-                    selectedElements.Add(oddElements[i]);    
-                }
+                if (list[i] % 2 != 0)
+                    selectedElements.Add(list[i]);
             }
             return selectedElements;
         }
